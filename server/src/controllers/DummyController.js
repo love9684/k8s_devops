9eebdbddb1b27e0f0a9df32768919845f70935e0
Lovelesh Kumar <agrawal.lovelesh@gmail.com>
2023-06-15 16:18:04 +0530

    Update DummyController.js

--- server/src/controllers/DummyController.js
+++ server/src/controllers/DummyController.js
@@ -3,7 +3,7 @@ const { getDB } = require('../db');
 exports.getDummyData = async (req, res) => {
     const db = getDB();
     const users = await db.collection('user').find().toArray();
-    res.send(users);
+    res.send(users.concat({name: 'Testing CI/CD'}));
     // res.send(["Deploy updated"]);
 }
 
