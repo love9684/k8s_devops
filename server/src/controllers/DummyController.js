7a7a5468b37b4f95f9e60feb1a7b655b9d3ca74e
love9684 <agrawal.lovelesh@gmail.com>
2023-06-15 16:29:32 +0530

    test

--- server/src/controllers/DummyController.js
+++ server/src/controllers/DummyController.js
@@ -3,8 +3,8 @@ const { getDB } = require('../db');
 exports.getDummyData = async (req, res) => {
     const db = getDB();
     const users = await db.collection('user').find().toArray();
-    res.send(users.concat({name: 'Testing CI/CD'}));
-    // res.send(["Deploy updated"]);
+    res.send(users);
+    // res.send(users.concat({name: 'Testing CI/CD'}));
 }
 
 exports.setDummyData = async (req, res) => {
